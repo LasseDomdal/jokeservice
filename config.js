f337180ef592c58aef269c9356eef2b518acd9ec
LasseDomdal <domdal.lasse@gmail.com>
2021-04-09 11:38:47 +0200

    Initial commit

---
+++ config.js
@@ -0,0 +1,7 @@
+// config.js
+const config = {
+    databaseURI: 'mongodb+srv://LasseDomdal:1234@clustertest.ozjwc.mongodb.net/ClusterTest?retryWrites=true&w=majority', // or MongoDB Atlas connection URI
+    localPort: 8080
+};
+
+module.exports = config;
---
+++ controllers/controller.js
@@ -0,0 +1,28 @@
+// controllers.js
+const mongoose = require("mongoose");
+const Company = require("../models/Joke");
+const config = require("../config");
+
+mongoose.connect(config.databaseURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+exports.createJoke = function (setup, punchline) {
+  return Joke.create({
+    setup,
+    punchline,
+  });
+};
+
+exports.createJoke("Hvordan får man en fisk til at grine?", "Man putter den i kildevand");
+exports.createJoke("Hvornår har bageren fødselsdag?", "Den 11/11");
+
+exports.getJoke = function (jokeId) {
+  return Joke.findById(jokeId).exec();
+};
+
+exports.getJokes = function () {
+  return Joke;
+};
+
