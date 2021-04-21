3c1c241fe5c27c38e16b688e3a97d99a7c83354e
William Husum <husum44@gmail.com>
2021-04-21 14:18:17 +0200

    tilføjet cors

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 // app.js
-const express = require('express');
+const express = require("express");
 const app = express();
-const config = require('./config');
+const config = require("./config");
+const cors = require("cors");
 
-app.use(express.static(__dirname + '/public'));
+app.use(cors());
+
+app.use(express.static(__dirname + "/public"));
 app.use(express.json());
-app.use('/api/jokes', require('./routes/joke'));
-app.use('/api/othersites', require('./routes/site'))
-app.use('/api/otherjokes', require('./routes/otherjokes'))
-
-
+app.use("/api/jokes", require("./routes/joke"));
+app.use("/api/othersites", require("./routes/site"));
+app.use("/api/otherjokes", require("./routes/otherjokes"));
 
 const port = process.env.PORT || config.localPort; // Heroku
 app.listen(port);
-console.log('Listening on port ' + port + ' ...');
+console.log("Listening on port " + port + " ...");
 
 module.exports = app; // test
